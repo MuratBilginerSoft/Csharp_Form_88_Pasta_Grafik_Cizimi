7d434d069f3e5c18db59225a2b45d870f8c6a99e
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 23:25:10 +0300

    Initial commit

---
+++ Pasta Grafik Çizimi/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Pasta_Grafik_Çizimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grb1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.grb1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grb1
+            // 
+            this.grb1.Controls.Add(this.button1);
+            this.grb1.Controls.Add(this.textBox4);
+            this.grb1.Controls.Add(this.textBox3);
+            this.grb1.Controls.Add(this.textBox2);
+            this.grb1.Controls.Add(this.textBox1);
+            this.grb1.Controls.Add(this.label5);
+            this.grb1.Controls.Add(this.label4);
+            this.grb1.Controls.Add(this.label3);
+            this.grb1.Controls.Add(this.label2);
+            this.grb1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grb1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.grb1.Location = new System.Drawing.Point(20, 25);
+            this.grb1.Name = "grb1";
+            this.grb1.Size = new System.Drawing.Size(170, 203);
+            this.grb1.TabIndex = 1;
+            this.grb1.TabStop = false;
+            this.grb1.Text = "Grafik Değerleri";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(6, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Grafik Çiz";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(84, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(66, 23);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(84, 99);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(66, 23);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(66, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(16, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "4. Değer:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(16, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "3. Değer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(16, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "2. Değer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(16, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "1. Değer:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(196, 191);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(321, 37);
+            this.panel1.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.Location = new System.Drawing.Point(269, 13);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 13);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "4. Değer";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.BurlyWood;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Location = new System.Drawing.Point(243, 11);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(20, 16);
+            this.button5.TabIndex = 26;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(189, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "3. Değer";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Blue;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(163, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(20, 16);
+            this.button4.TabIndex = 24;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(109, 11);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "2. Değer";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Cyan;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(83, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 16);
+            this.button3.TabIndex = 22;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(29, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "1. Değer";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(3, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 16);
+            this.button2.TabIndex = 20;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(529, 243);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.grb1);
+            this.Name = "Form1";
+            this.Text = "Pasta Grafik Çizimi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grb1.ResumeLayout(false);
+            this.grb1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Pasta Grafik Çizimi/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pasta_Grafik_Çizimi
+{
+    public partial class Form1 : Form
+    {
+        int a, b, c, d; // textbox tan gelen değerleri tutacakalar.
+
+        float d1, d2, d3, d4, toplam=0; // çeviri yapıldıktan sonra grafik değerlerini tuacak.
+
+        Pen p; // Çizim yapacağımız kalemi tanımladık.
+
+        Graphics g; // Çizim işlemini başlatabilmek için grafik nesnemizi tanımladık.
+
+        Rectangle r;
+
+        Brush b1, b2, b3, b4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            panel1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text.Trim());
+            b = int.Parse(textBox2.Text.Trim());
+            c = int.Parse(textBox3.Text.Trim());
+            d = int.Parse(textBox4.Text.Trim());
+
+            
+
+            toplam = a + b + c + d;
+
+            d1 = (a / toplam) * 360;
+            d2 = (b / toplam) * 360;
+            d3 = (c / toplam) * 360;
+            d4 = (d / toplam) * 360;
+
+
+            p = new Pen(Color.Yellow,2);
+
+            g = this.CreateGraphics();
+
+            
+
+            Rectangle rec = new Rectangle(280, 35, 150, 150);
+
+            b1 = new SolidBrush(Color.Red);
+            b2 = new SolidBrush(Color.Cyan);
+            b3 = new SolidBrush(Color.Blue);
+            b4 = new SolidBrush(Color.BurlyWood);
+
+           
+
+            g.DrawPie(p,rec,0,d1);
+            g.FillPie(b1, rec, 0, d1);
+            
+            g.DrawPie(p,rec,d1,d2);
+            g.FillPie(b2,rec,d1,d2);
+
+            g.DrawPie(p, rec, d1+d2, d3);
+            g.FillPie(b3, rec, d1+d2, d3); 
+
+            g.DrawPie(p, rec, d1+d2+d3, d4);
+            g.FillPie(b4, rec, d1+d2+d3, d2);
+
+
+            panel1.Visible = true;
+           
+            
+
+
+
+
+
+
+        }
+    }
+}
